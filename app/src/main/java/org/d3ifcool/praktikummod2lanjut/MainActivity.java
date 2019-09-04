dbe9780aece71b3ef2c10c8616df3e94d395e5ed
Rahmat Tobi Hidayat <tobytoby298@gmail.com>
2019-09-04 08:46:02 +0700

    initial commit

---
+++ app/src/main/java/org/d3ifcool/praktikummod2lanjut/MainActivity.java
@@ -0,0 +1,15 @@
+package org.d3ifcool.praktikummod2lanjut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
